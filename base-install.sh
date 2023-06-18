06a394a1ffb4b25dabb74c8f84c9e199ef32781b
Vinicius Yaunner <vinifysantos@gmail.com>
2023-06-18 21:34:43 +0000

    Update base-install.sh

--- base-install.sh
+++ base-install.sh
@@ -3,7 +3,6 @@ echo
 echo "init..."
 echo
 
-localectl br-abnt2
 
 #AUR
 mkdir Sources
@@ -15,7 +14,7 @@ makepkg -si
 # Color pacman
 echo -e "Color\r\nILoveCandy" >> /etc/pacman.conf
 
-yay -S pa-applet-git
+cd $HOME
 
 PKGS=(
         'xorg-server'           
@@ -24,19 +23,52 @@ PKGS=(
         'xf86-video-amdgpu'      
         'mesa'                  
         'xf86-input-libinput'  
-        'vulkan-radeon'
         'libva-mesa-driver'
         'mesa-vdpau'
         'lib32-mesa' 
         'bluez'
         'bluez-utils'
         'blueman'
-)
+        'code'
+        'i3-gaps'           
+        'i3ipc-glib-git' 
+        '3lock-color'            
+        'polybar'           
+        'network-manager-applet'      
+        'sddm'                  
+        'rofi'
+        'nitrogen'
+        'pulseaudio'
+        'pulseaudio-alsa' 
+        'python-pip'
+        'zsh'
+        'dunst'
+        'dolphin'
+        'picom'
+        'ranger'
+        'btop'
+        'terminus-font'
+        'gnome-screenshot'
+        'telegram-desktop'
+        'mpd'
+        'lxappearance'
+        'xfce4-terminal'
+        'xdg-user-dirs'
+        'opera'
+        'alsa-utils' 
+        'pavucontrol'
+        'w3m'
+        'ttf-font-awesome'
+        'pywal'
+        'xf86-input-synaptics'
+        )
 
 for PKG in "${PKGS[@]}"; do
     echo "INSTALLING: ${PKG}"
-    pacman -S "$PKG" --noconfirm --needed
+    sudo pacman -S "$PKG"
 done
+
+xdg-user-dirs-update
 
 #bluetooth
 sudo systemctl enable bluetooth
@@ -51,6 +83,21 @@ sudo systemctl enable bluetooth
 
 #SSD
 sudo systemctl enable fstrim.timer
+
+yay -S oh-my-zsh-git nerd-fonts-hack meowfetch pa-applet-git
+
+chsh -l
+chsh -s /usr/bin/zsh
+
+sudo git clone https://github.com/bhilburn/powerlevel9k.git /usr/share/oh-my-zsh/themes/powerlevel9k 
+
+cp .zshrc ~/.zshrc
+
+curl https://cypherpunks.com.br/artes/Artistas/Vinicius%20Yaunner/beauty-death.png > pywallbase.png
+curl https://cypherpunks.com.br/artes/Artistas/Vinicius%20Yaunner/flower-cypher.png > wallpaper.jpg
+
+mv pywallbase.png ~/Pictures
+mv wallpaper.jpg ~/Pictures
 
 echo
 echo "Done!"
